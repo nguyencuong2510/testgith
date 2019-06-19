0fd4e62da1eff8b15ec5d3c2a6a9d2cb86836356
cuong2510 <>
2019-06-19 11:23:11 +0700

    init project

---
+++ asdasdsad/asdasdsadTests/asdasdsadTests.swift
@@ -0,0 +1,34 @@
+//
+//  asdasdsadTests.swift
+//  asdasdsadTests
+//
+//  Created by PiraGo on 6/19/19.
+//  Copyright © 2019 PiraGo. All rights reserved.
+//
+
+import XCTest
+@testable import asdasdsad
+
+class asdasdsadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
